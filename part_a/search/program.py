7d288e3dfc3d3b2afd395301de1e001451260506
yeungeqq <eq.yeung@gmail.com>
2024-03-18 12:20:33 +1100

    Initial Commit
    Initial Commit

---
+++ part_a/search/program.py
@@ -0,0 +1,63 @@
+# COMP30024 Artificial Intelligence, Semester 1 2024
+# Project Part A: Single Player Tetress
+
+from .core import PlayerColor, Coord, PlaceAction
+from .utils import render_board
+
+
+def search(
+    board: dict[Coord, PlayerColor], 
+    target: Coord
+) -> list[PlaceAction] | None:
+    """
+    This is the entry point for your submission. You should modify this
+    function to solve the search problem discussed in the Part A specification.
+    See `core.py` for information on the types being used here.
+
+    Parameters:
+        `board`: a dictionary representing the initial board state, mapping
+            coordinates to "player colours". The keys are `Coord` instances,
+            and the values are `PlayerColor` instances.  
+        `target`: the target BLUE coordinate to remove from the board.
+    
+    Returns:
+        A list of "place actions" as PlaceAction instances, or `None` if no
+        solution is possible.
+    """
+
+    # The render_board() function is handy for debugging. It will print out a
+    # board state in a human-readable format. If your terminal supports ANSI
+    # codes, set the `ansi` flag to True to print a colour-coded version!
+    print(render_board(board, target, ansi=False))
+
+    # Do some impressive AI stuff here to find the solution...
+    # separate red and blue into two lists
+    red = []
+    blue = []
+    for coordination in board:
+        if board[coordination] is PlayerColor.RED:
+            red.append(coordination)
+        else:
+            blue.append(coordination)
+
+    # find out the goal state - all coords that need to be filled to eliminate target
+
+    # find out all possible ways to fulfill the goal state from the target
+            
+    # find out possible paths between the starting red points and the all consecutive lines of the goal state
+    # use breadth-first search
+    # if no possible path to one of the lines return None
+            
+    # connect lines to the starting red points if there are paths to all lines
+
+    # convert the PlaceAction backward
+
+    # Here we're returning "hardcoded" actions as an example of the expected
+    # output format. Of course, you should instead return the result of your
+    # search algorithm. Remember: if no solution is possible for a given input,
+    # return `None` instead of a list.
+    return [
+        PlaceAction(Coord(2, 5), Coord(2, 6), Coord(3, 6), Coord(3, 7)),
+        PlaceAction(Coord(1, 8), Coord(2, 8), Coord(3, 8), Coord(4, 8)),
+        PlaceAction(Coord(5, 8), Coord(6, 8), Coord(7, 8), Coord(8, 8)),
+    ]
